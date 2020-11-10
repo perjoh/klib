28a497e36f093b9994571e3ca54cb92768534466
perjoh <perjoh@users.noreply.github.com>
2020-11-10 09:52:16 +0100

    rpi

--- include/klib/file_io.hpp
+++ include/klib/file_io.hpp
@@ -1,8 +1,39 @@
 #pragma once
 #include <string>
+#include <fstream>
 
 namespace klib {
+namespace io {
 
     std::string read_textfile(const char* filename);
 
+    // 
+    class Resource
+    {
+    public :
+        virtual ~Resource() {}
+
+        virtual std::size_t read(void* dest, std::size_t bytes) = 0;
+
+    private :
+    };
+
+    // 
+    class Resource_file : public Resource
+    {
+    public : 
+        Resource_file(const std::string& filename, size_t flags = std::ios::binary);
+
+    public :
+        std::size_t read(void* dest, std::size_t bytes) override;
+        std::size_t size() const;
+
+        std::istream& stream();
+
+    private :
+        std::ifstream file_;
+        std::size_t filesize_;
+    };
+
+} // namespace io
 } // namespace klib
--- src/klib/file_io.cpp
+++ src/klib/file_io.cpp
@@ -1,22 +1,74 @@
 #include <klib/file_io.hpp>
-#include <fstream>
+#include <filesystem>
+#include <cassert>
 
 namespace klib {
+namespace io {
+
+    class Text_file : Resource_file
+    {
+    public :
+        Text_file(const std::string& filename) : Resource_file(filename, std::ios::in)
+        { }
+
+        std::string contents()
+        {
+            std::string str;
+            str.resize(this->size());
+            read(&str[0], str.size());
+            str.push_back('\0');
+            return str;
+        }
+
+    private :
+    }; 
 
     std::string read_textfile(const char* filename)
     {
-        std::string result;
+        Text_file tf(filename);
+        return tf.contents();
+    } 
 
-        std::ifstream file(filename, std::ios::binary | std::ios::ate);
-        if (file.is_open()) 
-        {
-            const size_t filesize = static_cast<size_t>(file.tellg());
-            file.seekg(0, std::ios::beg);
-            result.resize(filesize);
-            file.read(&result[0], filesize);
-        }
+    Resource_file::Resource_file(const std::string& filename, size_t flags)
+        : filesize_(0)
+    {
+        // If RESPATH exists, use it as a path.
+        const char* respath = std::getenv("RESPATH"); 
+        std::filesystem::path filepath = respath ? std::filesystem::path(respath) / filename 
+                                                 : std::filesystem::path(filename);
 
-        return result;
+        flags |= std::ios::ate;
+
+        file_.open(filepath.c_str(), static_cast<std::ios_base::openmode>(flags));
+        if (file_.is_open()) {
+            filesize_ = static_cast<std::size_t>(file_.tellg());
+            file_.seekg(0, std::ios::beg);
+        } 
     }
 
+    std::size_t Resource_file::read(void* dest, std::size_t bytes)
+    {
+        assert(bytes > 0);
+        assert(dest);
+
+        auto pos = file_.tellg();
+        file_.read(reinterpret_cast<char*>(dest), bytes);
+        if (file_.good() || file_.eof()) {
+            return file_.tellg() - pos;
+        }
+
+        return 0;
+    }
+
+    std::size_t Resource_file::size() const
+    {
+        return filesize_;
+    }
+
+    std::istream& Resource_file::stream()
+    {
+        return file_;
+    }
+
+} // namespace io
 } // namespace klib
