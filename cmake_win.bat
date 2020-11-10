2ad8158244aba220db4caca99480b0ce99cf4cf3
perjoh <perjoh@users.noreply.github.com>
2020-11-10 14:40:21 +0100

    rpi

--- cmake_win.bat
+++ cmake_win.bat
@@ -1,6 +1,7 @@
 mkdir _build
 pushd _build
 rem Note: Run 'vcpkg integrate install' to find path to vcpkg.cmake.
-set CMAKE_TOOLCHAIN_FILE=C:/bin/vcpkg/scripts/buildsystems/vcpkg.cmake
+rem set CMAKE_TOOLCHAIN_FILE=C:/bin/vcpkg/scripts/buildsystems/vcpkg.cmake
+echo %CMAKE_TOOLCHAIN_FILE%
 cmake -DCMAKE_TOOLCHAIN_FILE=%CMAKE_TOOLCHAIN_FILE% -DCMAKE_GENERATOR_PLATFORM=x64 ..\
 popd
---
+++ setup_vcpkg.bat
@@ -0,0 +1,5 @@
+set VCPKG_DEFAULT_TRIPLET=x64-windows
+vcpkg install catch2
+vcpkg install nlohmann-json
+vcpkg install glm
+vcpkg install glew
